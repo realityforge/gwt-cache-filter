b5fbd185c3373d5d958f2f9dc66f4989cde46b47
Peter Donald <peter@realityforge.org>
2020-03-06 11:24:52 +1100

    Remove unnecessary throws

--- src/main/java/org/realityforge/gwt/cache_filter/AbstractFilter.java
+++ src/main/java/org/realityforge/gwt/cache_filter/AbstractFilter.java
@@ -2,14 +2,12 @@ package org.realityforge.gwt.cache_filter;
 
 import javax.servlet.Filter;
 import javax.servlet.FilterConfig;
-import javax.servlet.ServletException;
 
 abstract class AbstractFilter
   implements Filter
 {
   @Override
   public void init( final FilterConfig config )
-    throws ServletException
   {
   }
 
